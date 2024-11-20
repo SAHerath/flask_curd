a24dbd0633e2dbca791cb5fbac403cd05a81f1b5
SAHerath <h.supun.anuradha@gmail.com>
2024-11-20 11:10:13 +0530

    initial app

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # Import libraries
+from flask import Flask, redirect, request, render_template, url_for
 
 # Instantiate Flask functionality
+app = Flask(__name__)
 
 # Sample data
 
@@ -13,4 +15,6 @@
 # Delete operation
 
 # Run the Flask app
+if __name__ == "__main__":
+    app.run(debug=True)
     
